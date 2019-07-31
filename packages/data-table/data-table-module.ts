59cd75eb35a32a428efe31bf7cdf731a6cd657c5
Dominic Carretto <trimoxal@gmail.com>
2019-07-31 18:02:08 -0400

    WIP: data-table

---
+++ packages/data-table/data-table-module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+export const DATA_TABLE_DECLARATIONS = [
+];
+
+@NgModule({
+  imports: [CommonModule],
+  exports: DATA_TABLE_DECLARATIONS,
+  declarations: DATA_TABLE_DECLARATIONS
+})
+export class MdcDataTableModule { }
